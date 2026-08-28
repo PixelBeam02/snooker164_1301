a239bc740fa3d22350a7827a36c59a3d75c55d42
PixelBeam02 <nakanya0812@gmail.com>
2026-08-28 20:00:59 +0700

    Finish Snooker

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +15,21 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject ballPrefab;
 
+    [SerializeField]
+    private GameObject cueBall;
+
+    [SerializeField]
+    private float xInput = 0f;
+
+    [SerializeField]
+    private GameObject ballLine;
+
+    [SerializeField]
+    private GameObject cam;
+
+    [SerializeField]
+    private TMP_Text notiText;
+
     public static GameManager instance;
 
     void Awake()
@@ -22,6 +39,8 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        CameraBehindCueBall();
+
         SetBall(BallColor.Red, 1);
         SetBall(BallColor.Yellow, 2);
         SetBall(BallColor.Green, 3);
@@ -35,7 +54,19 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RotateBall();
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            ShootBall();
+
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+            xInput = -0.05f;
+        else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+            xInput = 0.05f;
+        else
+            xInput = 0f;
+        if (Keyboard.current.backspaceKey.wasPressedThisFrame)
+            StopBall();
     }
 
     private void SetBall(BallColor col, int i)
@@ -46,5 +77,48 @@ public class GameManager : MonoBehaviour
 
         Ball b = obj.GetComponent<Ball>();
         b.SetColorAndPoint(col);
+    }
+
+    private void ShootBall()
+    {
+        Rigidbody rd = cueBall.GetComponent<Rigidbody>();
+        rd.AddRelativeForce(Vector3.forward * 50, ForceMode.Impulse);
+
+        ballLine.SetActive(false);
+        cam.transform.parent = null;
+        cam.transform.position = new Vector3(0f, 30f, -42f);
+        cam.transform.eulerAngles = new Vector3(45f, 0f , 0f);
+    }
+    private void RotateBall()
+    {
+        if (cueBall != null)
+            cueBall.transform.Rotate(new Vector3(0f, xInput, 0f));
+    }
+
+    private void StopBall()
+    {
+        Rigidbody rb = cueBall.GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        cueBall.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+
+        ballLine.SetActive(true);
+        CameraBehindCueBall();
+    }
+
+    private void CameraBehindCueBall()
+    {
+        cam.transform.parent = cueBall.transform;
+        cam.transform.position = cueBall.transform.position + new Vector3(0f, 7f, -15f);
+        cam.transform.eulerAngles = new Vector3(30f, 0f, 0f);
+    }
+    public void ShowScoreText(int n)
+    {
+        Playerscore += n;
+        notiText.text = $"Ball Point:{n}\nTotal Score:{Playerscore}";
+    }
+    public void ShowString(string s)
+    {
+        notiText.text = s;
     }
 }
